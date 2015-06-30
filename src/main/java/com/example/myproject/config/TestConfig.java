ff491f2fbe720ee227f896dd7f3988822a65d2d9
jinweile <jinweile@shangpin.com>
2015-06-30 10:47:37 +0800

    增加上传文件演示功能

--- src/main/java/com/example/myproject/config/TestConfig.java
+++ src/main/java/com/example/myproject/config/TestConfig.java
@@ -2,14 +2,20 @@ package com.example.myproject.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.servlet.MultipartConfigElement;
+
 /**
  * Created by jinweile on 2015/6/26.
  */
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.example.myproject.web","com.example.myproject.db"})
 @PropertySource("classpath:resources.properties")
@@ -18,5 +24,13 @@ public class TestConfig {
 
     @Autowired
     private Environment env;
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("1Mb");
+        factory.setMaxRequestSize("10Mb");
+        return factory.createMultipartConfig();
+    }
 
 }
--- src/main/java/com/example/myproject/web/Index.java
+++ src/main/java/com/example/myproject/web/Index.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,6 +46,25 @@ public class Index {
         model.addAllObjects(map);
         model.setViewName("test");
         return model;
+    }
+
+    @RequestMapping(value="/upload", method= RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                                                  @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                file.getName();
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("D:\\temp\\" + name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
     }
 
 }
